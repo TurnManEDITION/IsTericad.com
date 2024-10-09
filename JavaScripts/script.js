638238f970c2fb68a90a3e494245311bf72753e0
Andrey Dorofeev <116450431+TurnManEDITION@users.noreply.github.com>
2024-10-09 08:44:57 +0300

    Add files via upload

--- JavaScripts/script.js
+++ JavaScripts/script.js
@@ -1,15 +1,9 @@
-// function myFunction() {
-//   var copyText = document.getElementById("xmp");
-//   copyText.select();
-//   document.execCommand("copy");
-//   alert("Copied the text: " + copyText.value);
-// }
 function myFunction() {
   var range = document.createRange();
   range.selectNode(document.getElementById("xmp"));
-  window.getSelection().removeAllRanges(); // clear current selection
-  window.getSelection().addRange(range); // to select text
+  window.getSelection().removeAllRanges();
+  window.getSelection().addRange(range);
   document.execCommand("copy");
-  window.getSelection().removeAllRanges();// to deselect
+  window.getSelection().removeAllRanges();
   alert("Код скопирован!")
 }

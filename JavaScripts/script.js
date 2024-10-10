7483a09fdddff634c13652a350bfcee2fd7232bc
Andrey Dorofeev <116450431+TurnManEDITION@users.noreply.github.com>
2024-10-10 07:51:34 +0300

    Add files via upload

--- JavaScripts/script.js
+++ JavaScripts/script.js
@@ -7,3 +7,19 @@ function myFunction() {
   window.getSelection().removeAllRanges();
   alert("Код скопирован!")
 }
+document.querySelector('#button_menu').addEventListener('click', () => {
+  document.querySelector('#content').classList.remove('display_block');
+  document.querySelector('#content').classList.add('display_none');
+  document.querySelector('#content_menu').classList.remove('display_none');
+  document.querySelector('#content_menu').classList.add('display_block');
+  document.querySelector('#header_content').classList.remove('display_block');
+  document.querySelector('#header_content').classList.add('display_none');
+})
+document.querySelector('#button_back').addEventListener('click', () => {
+  document.querySelector('#content').classList.remove('display_none');
+  document.querySelector('#content').classList.add('display_block');
+  document.querySelector('#content_menu').classList.remove('display_block');
+  document.querySelector('#content_menu').classList.add('display_none');
+  document.querySelector('#header_content').classList.remove('display_none');
+  document.querySelector('#header_content').classList.add('display_block');
+})
